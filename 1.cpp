2d5ec1558bfe94bf05a48e84fcf7a834a5de85d3
TonyJM <53893112+TonyJM@users.noreply.github.com>
2019-11-29 23:29:06 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -14,13 +14,15 @@ void lcs( string X, string Y, int m, int n ){
         }
     }
     int index = L[m][n];
-    cout<<"Length:"<<L[m+1][n+1]<<endl;
-    char lcs[index+1];
-    lcs[index] = '\0';
+    cout<<"Length:"<<L[m][n]<<endl;
+    //char lcs[index+1];
+    //lcs[index] = '\0';
+    string lcs="";
     int i = m, j = n;
     while (i > 0 && j > 0){
         if (X[i-1] == Y[j-1]){
-            lcs[index-1] = X[i-1];
+            //lcs[index-1] = X[i-1];
+            lcs=X[i-1]+lcs;
             i--; j--; index--;
         }
         else if (L[i-1][j] > L[i][j-1])

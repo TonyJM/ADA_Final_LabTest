611015ea4709a0fb283df1c941cb5873105cd1b1
TonyJM <53893112+TonyJM@users.noreply.github.com>
2019-11-30 06:41:39 +0530

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -47,6 +47,7 @@ int main(){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cin>>graph[i][j];
+	    //if(graph[i][j]==-1) graph[i][j]=0;
         }
     }
     cout<<"\nEnter source : ";
